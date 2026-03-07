4caf26090930b5673ecbfd0746c8beb9049bd694
TOMINAGA HAYATO <tominaga_h@mad2007.co.jp>
2026-03-07 19:10:29 +0900

    モックデータをYAMLで設定できるように

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,25 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  turbopack: {
+    rules: {
+      "*.yaml": {
+        loaders: ["yaml-loader"],
+        as: "*.js",
+      },
+      "*.yml": {
+        loaders: ["yaml-loader"],
+        as: "*.js",
+      },
+    },
+  },
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.ya?ml$/,
+      use: "yaml-loader",
+    });
+    return config;
+  },
 };
 
 export default nextConfig;
